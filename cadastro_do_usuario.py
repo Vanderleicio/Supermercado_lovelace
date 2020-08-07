8ee0c1a7e00d02afd690d3dfbd332abc482dfd38
bielcmoraes <gcmorais66@github.com>
2020-08-07 16:32:58 -0300

    Cadastro de usuario melhorado

--- cadastro_do_usuario.py
+++ cadastro_do_usuario.py
@@ -1,17 +1,34 @@
 #Menu de cadastro do cliente
 def cadastro():
     cliente = {}
-
     #Organiza os dados informados pelo cliente em um dicionário
     nomeCliente = input(f"Nome Completo: ")
     cliente[f"Nome"] = nomeCliente
 
     emailCliente = input(f"Email: ")
-    cliente[f"Email"] = emailCliente
+    emailCompare = emailCliente.count('@')
+    if emailCompare != 0:
+        cliente[f"Email"] = emailCliente
+    else:
+        print(f"Email com formato inválido. Tente novamente.")
+        return
 
+
+    print(f"A senha deve ser composta de uma letra maiuscúla e um número.")
     senhaCliente = input(f"Digite uma senha: ")
-    cliente[f"Senha"] = senhaCliente
+    senhaMinuscula = senhaCliente.islower()
+    print(senhaMinuscula)
 
+    if len(senhaCliente) > 6:
+            if senhaMinuscula == True:
+                print(f"A senha deve ter pelo menos uma letra maiuscúla.")
+                return
+
+            else:
+                cliente[f"Senha"] = senhaCliente
+    else:
+        print(f"A senha deve ter no mínimo 6 caractéres.")
+        return
 
     telefoneCliente = input(f"Telefone: ")
     cliente[f"Telefone"] = telefoneCliente
